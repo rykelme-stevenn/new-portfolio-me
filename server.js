f9c133f82d9b0eb48941e172957379fde9599474
rykelme-stevenn <rykelmesteven@outlook.com>
2023-11-07 22:33:14 -0300

    add

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8080; // Usa a variável de ambiente PORT ou a porta 8080 se não definida.
 
-const path = require('path')
+const appName = 'new-portfolio-app';
+const staticDir = path.join(__dirname, 'dist', appName); // Caminho para o diretório de arquivos estáticos.
 
-const app = express()
+app.use(express.static(staticDir));
 
-const appName = 'new-portfolio-app'
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(staticDir, 'index.html'));
+});
 
-app.use(express.static( __dirname + `/dist/${appName}`))
-
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + `/dist/${appName}/index.html`))
-})
-
-app.listen(8080)
+app.listen(port, () => {
+    console.log(`Aplicativo está rodando na porta ${port}`);
+});
